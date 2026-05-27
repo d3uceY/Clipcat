92349f02feae5fb4cdd48cb43d951ac8b58afe6a
d3uceY <onyekwelujesse1234@gmail.com>
2026-05-27 13:43:11 +0100

    feat: Adjust settings dialog width for improved layout on smaller screens

--- frontend/src/components/window-controls.tsx
+++ frontend/src/components/window-controls.tsx
@@ -230,7 +230,7 @@ export default function WindowControls() {
                 </button>
                 <div ref={settingDialogRef} className="fixed inset-0 z-50 flex items-center justify-center">
                     <div className="absolute inset-0 bg-black/30" onClick={handleSettingsClick} />
-                    <div ref={settingDialogInnerRef} className="setting-dialog relative z-10 w-[90vw] max-w-lg rounded-sm border-0 h-screen! sm:h-[90vh]! max-h-100">
+                    <div ref={settingDialogInnerRef} className="setting-dialog relative z-10 w-[90vw] max-w-sm rounded-sm border-0 h-screen! sm:h-[90vh]! max-h-100">
                         <button
                             onClick={handleSettingsClick}
                             className="absolute top-3 bg-[#F8F5F0] right-3 z-10 w-7 h-7 flex items-center justify-center hand-drawn-btn lined thin text-sm font-bold hover:opacity-70"
